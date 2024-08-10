d10b7791c56172ffcb1a73f58f9485890b20f25e
git-dis-git-dat <too5juan@protonmail.com>
2024-08-10 12:10:35 -0400

    basic html

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function divide(a, b){
     return a / b;
 }
 
-function posNegToggle(a){
+function inverse(a){
     if (a > 0){
         return a * -1;
     }  else {
@@ -24,6 +24,30 @@ function posNegToggle(a){
     }
 }
 
-function toPercent(a){
+function percent(a){
     return a / 100;
 }
+// variables
+
+const operator = null;
+const num1 = 0;
+const num2 = 0;
+
+// 
+function operate(operator, num1, num2){
+    switch (operator){
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+        case 'multiply':
+            return multiply(num1, num2);
+        case 'divide':
+            return divide(num1, num2);
+        case 'inverse':
+            return inverse(num1);
+        case 'percent':
+            return percent(num1);
+    }    
+}
+

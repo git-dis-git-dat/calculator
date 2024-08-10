e49c0ee120e59f577a209738cde1fa939d70b5af
git-dis-git-dat <too5juan@protonmail.com>
2024-08-10 11:10:39 -0400

    operations

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert('hola mundo');
+// operations
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function posNegToggle(a){
+    if (a > 0){
+        return a * -1;
+    }  else {
+        return Math.abs(a);
+    }
+}
+
+function toPercent(a){
+    return a / 100;
+}

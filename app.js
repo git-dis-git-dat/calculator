f0862afb3fa63ae6cb8fcd10920b69101a7f9f2b
git-dis-git-dat <too5juan@protonmail.com>
2024-08-13 18:10:53 -0400

    updateDisplay w decimal support

--- app.js
+++ app.js
@@ -30,9 +30,10 @@ function percent(a){
 // variables
 
 let operator = null;
-let num1 = [];
-let num2 = [];
+let num1 = 0;
+let num2 = 0;
 // const result = 0;
+let displayValue = [];
 
 // 
 function operate(operator, num1, num2){
@@ -52,41 +53,24 @@ function operate(operator, num1, num2){
     }    
 }
 
-function setInput(button){
-    if(button.classList.contains('num') && !operator){
-        // if(num1.length === 0){num1.push(button.id.slice(1))}
-        num1.push(button.id.slice(1))
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((el)=>el.addEventListener('click', ()=>updateDisplay(el)));
+
+function updateDisplay(button){
+    let display = document.querySelector('#display');
+    if(button.classList.contains('num')){
+        displayValue.push(button.id.slice(1));
+        display.textContent = displayValue.join('');
+        console.log(displayValue)
     }
-    if(button.classList.contains('operator')){
-        operator = button.id;
-        num1 = parseInt(num1.join(''));
-    }
-    if(button.classList.contains('num') && operator){
-        num2.push(button.id.slice(1))
-    }
-    if(button.id === 'equals'){
-        if(typeof num2 !== 'number'){num2 = parseInt(num2.join(''))};
-        num1 = operate(operator, num1, num2)
-        //num2 = [];
-        console.log(num1)
+    if(button.id === 'decimal' && !displayValue.includes('.')){
+        displayValue.push(button.textContent);
+        display.textContent = displayValue.join('');
     }
 }
 
 // split handling array to num and have seperate equals funtion that only works on numbers?
 
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach((el)=>el.addEventListener('click', ()=>setInput(el)))
 
 
-// push numbers into num1 [] when an operator is pressed join and parseInt
 
-// listen to clicks on divs and get the text content and convert to number (int or float?) or operator and return them for use in the operate function
-    // an ok side effect of getting text is that this might be a good spot to handle getting a decimal point(and disabling it from being entered again (unless it's on a new number)) before converting to num
-
-// inverse and percent only require one number. they should fire off operate when they are pressed
-
-// otherwise pressing equals should fire off operate on whatever is in the global variables
-    // if there is only one value pressing equals should do nothing
-    // if a valid operation has been performed pressing equals again should perform the last operation on the last result. this result could be a new variable, or it could take the place of num1. pressing an operator or equals should use the result var or new num1. pressing a number should clear the global vars
-
-//need to decide on clear behavior. calcs often have two buttons, or toggle one to "C" clear last value and keep previous value and AC to clear all values. apple if num1, then operator pressed, then num2 entered then C'd uses num1 as num2 if equals pressed
